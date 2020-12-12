fcf28bc628dee967db01f4ed0f681f62edc69872
ju5tu5 <justus@ju5tu5.nl>
2020-12-12 08:30:47 +0100

    setup

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<h1>Hello, {name}!</h1>
+
+<p>My 2nd Svelte app</p>
+
+<style>
+  p {
+     color: #1d4585; 
+     text-decoration: underline;
+  }
+</style>
+
+<script>
+  export let name = ''
+</script>
